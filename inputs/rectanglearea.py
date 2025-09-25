5626f4a15f585e6d9894d4f107ffddcfb58c1268
ShannonKendi <shannon.kihara@strathmore.edu>
2025-09-25 12:13:22 +0300

    first commit

---
+++ inputs/rectanglearea.py
@@ -0,0 +1,5 @@
+#Exercise 1: Rectangle Area Calc Area=length*width
+length=int(input("What is the length: "))
+width=int(input("What is the width: "))
+area=length*width
+print(f"The area is {area}cm")
---
+++ inputs/shoppingcart.py
@@ -0,0 +1,6 @@
+product=input("What would you like to buy? ")
+quantity=float(input("How many grams? "))
+price=float(input("What is the price? "))
+total = quantity*price
+
+print(f"You would like {product} of {quantity} grams and the total will be {total}")
